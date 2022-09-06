b55bf296272452cc37f39e961d8440e449d12b8a
Leobel Izquierdo <leobel@tangocrypto.com>
2022-09-06 15:46:14 -0300

    init commit

---
+++ src/apis/index.ts
@@ -0,0 +1,16 @@
+/* tslint:disable */
+/* eslint-disable */
+export * from './AddressesApi';
+export * from './AssetsApi';
+export * from './BlocksApi';
+export * from './EpochsApi';
+export * from './NFTsAddressListApi';
+export * from './NFTsCollectionsApi';
+export * from './NFTsSalePhasesApi';
+export * from './NFTsSalesApi';
+export * from './NFTsTokensApi';
+export * from './PolicyApi';
+export * from './PoolsApi';
+export * from './TransactionsApi';
+export * from './WalletsApi';
+export * from './WebhooksApi';
